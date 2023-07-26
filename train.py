27363ad6acc7ad5995cc4a5dac811738a08a94bb
briggsrr <robert.briggs.3@icloud.com>
2023-07-25 23:20:15 -0700

    bb - finishing train

--- train.py
+++ train.py
@@ -15,8 +15,6 @@ def set_seed(seed):
     np.random.seed(seed)
 set_seed(420)
 
-def get_mape(y_true, y_pred): 
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 df = pd.read_csv('CCPP_full_data.csv')
 
@@ -43,28 +41,30 @@ cv = KFold(n_splits=5, random_state=420, shuffle=True)
 # Training model1 with n_estimators=100 and max_depth=5
 rf1 = RandomForestRegressor(n_estimators=100, max_depth=5, random_state=420)
 cv_scores1 = cross_val_score(rf1, X_train, y_train, cv=cv, scoring='neg_mean_absolute_error')
-mape1 = -cv_scores1.mean()  # cv_scores will be negative because of the 'neg_mean_absolute_error' scoring
+mae1 = -cv_scores1.mean()  # cv_scores will be negative because of the 'neg_mean_absolute_error' scoring
 
 # Training model2 with n_estimators=200 and max_depth=10
 rf2 = RandomForestRegressor(n_estimators=200, max_depth=10, random_state=420)
 cv_scores2 = cross_val_score(rf2, X_train, y_train, cv=cv, scoring='neg_mean_absolute_error')
-mape2 = -cv_scores2.mean()
+mae2 = -cv_scores2.mean()
 
 # Training model3 with n_estimators=300 and max_depth=15
 rf3 = RandomForestRegressor(n_estimators=300, max_depth=15, random_state=420)
 cv_scores3 = cross_val_score(rf3, X_train, y_train, cv=cv, scoring='neg_mean_absolute_error')
-mape3 = -cv_scores3.mean()
+mae3 = -cv_scores3.mean()
 
-print(f"Cross-validated MAPE for model 1: {mape1} %")
-print(f"Cross-validated MAPE for model 2: {mape2} %")
-print(f"Cross-validated MAPE for model 3: {mape3} %")
+print(f"Cross-validated MAE for model 1: {mae1}")
+print(f"Cross-validated MAE for model 2: {mae2}")
+print(f"Cross-validated MAE for model 3: {mae3}")
 
-best_mape = min([mape1, mape2, mape3])
-if best_mape == mape1:
+best_mape = min([mae1, mae2, mae3])
+if best_mape == mae1:
     best_model = rf1
-elif best_mape == mape2:
+elif best_mape == mae2:
     best_model = rf2
 else:
     best_model = rf3
 
+
+best_model.fit(X_train, y_train)
 dump(best_model, 'best_model.joblib') 
