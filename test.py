5402d46029ea89ad631339041b0a5de5b960afea
briggsrr <robert.briggs.3@icloud.com>
2023-07-25 23:20:04 -0700

    finishing test

--- test.py
+++ test.py
@@ -3,22 +3,17 @@ from joblib import load
 import numpy as np
 
 # Function to calculate MAPE
-def get_mape(y_true, y_pred): 
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+def get_mae(y_true, y_pred): 
+     return np.mean(np.abs(y_true - y_pred))
 
-# Load the model
 best_model = load('best_model.joblib')
 
 # Load the test data
 X_test = pd.read_csv('X_test.csv')
-y_test = pd.read_csv('y_test.csv')  # This will return a DataFrame
-
-# Convert y_test to a 1-D array, since your model will predict a 1-D array
-y_test = y_test.values.flatten()
-
-# Predict on test data
+y_test = pd.read_csv('y_test.csv')
+y_test = y_test.iloc[:, 0] 
+# Make predictions with the best model
 y_pred_test = best_model.predict(X_test)
+final_mae = get_mae(y_test, y_pred_test)
 
-# Calculate and print final MAPE
-final_mape = get_mape(y_test, y_pred_test)
-print(f"Final MAPE on test set: {final_mape} %")
+print(f"Final MAE on test set: {final_mae} ")
